f10ea7ea9b0d54746a19d356c4f65f9bb98d75ec
G.S. Prayoga <satria.prayoga@gmail.com>
2018-07-30 15:58:44 +0700

    m

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -24,7 +24,7 @@
                                      <div class="form-group">
                                         <label for="email">
                                             Email Anda
-                                            <span class="asterix">*</span>
+                                             <span class="asterix">*</span>
                                         </label>
                                         <input type="email" name="email" class="form-control" >
                                     </div>
